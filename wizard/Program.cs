a254627e941f0495ec777a8296902d250510d3db
a796022 <796022@unizar.es>
2024-10-03 08:56:39 +0200

    Mostrar ventana

--- wizard/Program.cs
+++ wizard/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Gtk;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Application.Init();
+        
+        // Crear la ventana principal
+        Window ventana = new Window("Mi ventana GTK");
+        ventana.SetDefaultSize(400, 300);
+        ventana.DeleteEvent += (sender, e) => Application.Quit();
+
+        // Mostrar la ventana
+        ventana.ShowAll();
+
+        // Iniciar el bucle principal de GTK
+        Application.Run();
+    }
+}
