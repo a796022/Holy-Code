59e3db1d955f2c4f1e8f52918e592d9f88be1048
a796022 <796022@unizar.es>
2024-10-06 11:19:01 +0200

    Establecido el mínimo redimensionamiento d ela ventana

--- wizard/GUI.cs
+++ wizard/GUI.cs
@@ -6,8 +6,10 @@ namespace GUI
     {
         // Default main window properties
         private const string MAIN_WINDOW_DEFAULT_TITLE = "New Tree - Wizard";
-        private const int MAIN_WINDOW_DEFAULT_WIDTH = 400;
-        private const int MAIN_WINDOW_DEFAULT_HEIGHT = 300;
+        private const int MAIN_WINDOW_DEFAULT_WIDTH = 1200;
+        private const int MAIN_WINDOW_DEFAULT_HEIGHT = 700;
+        private const int MAIN_WINDOW_DEFAULT_MIN_WIDTH = 400;
+        private const int MAIN_WINDOW_DEFAULT_MIN_HEIGHT = 300;
 
         public GUI()
         {
@@ -20,6 +22,8 @@ namespace GUI
             Window window = new Window(MAIN_WINDOW_DEFAULT_TITLE);
             window.SetDefaultSize(MAIN_WINDOW_DEFAULT_WIDTH,
                                   MAIN_WINDOW_DEFAULT_HEIGHT);
+            window.SetSizeRequest(MAIN_WINDOW_DEFAULT_MIN_WIDTH,
+                                  MAIN_WINDOW_DEFAULT_MIN_HEIGHT);
             
             // Sets the behavior of the window in case of different events
             window.DeleteEvent += (sender, e) => Application.Quit();
