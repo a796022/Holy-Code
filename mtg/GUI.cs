50d7b67786445350ea83ec95ea37bb3dbc06b930
a796022 <796022@unizar.es>
2024-10-07 12:02:31 +0200

    Obtener datos del usuario y nuevos frames pequeños

--- mtg/GUI.cs
+++ mtg/GUI.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.InteropServices;
 
 namespace GUI
 {
@@ -12,17 +13,63 @@ namespace GUI
         private const string BIG_FRAME_LINE_END = " ║";
 
         private const string SMALL_FRAME_INIT =
-            "┌──────────────────────────────────────────────────────────────────────────────┐";
+            "║┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓║";
         private const string SMALL_FRAME_END =
-            "└──────────────────────────────────────────────────────────────────────────────┘";
-        private const string SMALL_FRAME_LINE_INIT = "│ ";
-        private const string SMALL_FRAME_LINE_END = " │";
+            "║┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛║";
+        private const string SMALL_FRAME_LINE_INIT = "║┃ ";
+        private const string SMALL_FRAME_LINE_END = " ┃║";
+
+        private const int CONSOLE_WIDTH = 80;
 
         public GUI() {}
 
         public void SayWelcome(string userName)
         {
             WriteBigDialog("Welcome to MTG Console, " + userName + "!");
+        }
+
+        public string AskUsername()
+        {
+            return MakeQuestion("Hi! What's your name?", true, new string[] { });
+        }
+
+        private string MakeQuestion(string question, bool avoidEmpty, string[] options)
+        {
+            Console.WriteLine("[Game]: " + question);
+            for (int i = 0; i < options.Length; i++)
+            {
+                string questionStr = "  " + (i + 1) + ". " + options[i] + "\n";
+                Console.WriteLine(questionStr);
+            }
+            
+            // Get the input and check if it's valid
+            Console.Write(">> ");
+            string? input = Console.ReadLine();
+            string answer;
+
+            if (options.Length > 0)
+            {
+                int option;
+                while (!int.TryParse(input, out option) || option < 1 || option > options.Length)
+                {
+                    Console.Write(">> ");
+                    input = Console.ReadLine();
+                }
+
+                answer = options[option - 1];
+            }
+            else
+            {
+                while (avoidEmpty && string.IsNullOrEmpty(input))
+                {
+                    Console.Write(">> ");
+                    input = Console.ReadLine();
+                }
+
+                answer = input ?? "";
+            }
+
+            return answer;
         }
 
         private void WriteBigDialog(string line)
@@ -39,13 +86,13 @@ namespace GUI
 
         private void EndBigDialog()
         {
-            Console.WriteLine(BIG_FRAME_END + "\n");
+            Console.WriteLine(BIG_FRAME_END);
         }
 
         private void WriteBigDialogLine(string line)
         {
-            // Divide the line in multiple lines of 76 characters
-            const int CHUNK_SIZE = 76;
+            // Divide the line in multiple lines of characters
+            const int CHUNK_SIZE = CONSOLE_WIDTH - 4;
             List<string> lines = new List<string>();
 
             for (int i = 0; i < line.Length; i += CHUNK_SIZE)
@@ -86,8 +133,8 @@ namespace GUI
 
         private void WriteSmallDialogLine(string line)
         {
-            // Divide the line in multiple lines of 36 characters
-            const int CHUNK_SIZE = 76;
+            // Divide the line in multiple lines of characters
+            const int CHUNK_SIZE = CONSOLE_WIDTH - 6;
             List<string> lines = new List<string>();
 
             for (int i = 0; i < line.Length; i += CHUNK_SIZE)
--- mtg/User.cs
+++ mtg/User.cs
@@ -1,13 +1,16 @@
+using GUI;
 using System;
 
 namespace MTG
 {
     public class User
     {
+        private GUI.GUI gui;
         private string name;
 
         public User()
         {
+            gui = new GUI.GUI();
             name = LoadName();
         }
 
@@ -27,7 +30,12 @@ namespace MTG
             }
             else
             {
-                return "User";
+                string username = gui.AskUsername();
+
+                Directory.CreateDirectory(Path.GetDirectoryName(nameFilePath) ?? "");
+                File.WriteAllText(nameFilePath, username);
+
+                return username;
             }
         }
     }
