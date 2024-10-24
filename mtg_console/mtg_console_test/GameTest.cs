223d5e27acea3c76e125eb74d8e70b791b3b14da
a796022 <796022@unizar.es>
2024-10-24 13:44:01 +0200

    Nuevos tests

--- mtg_console/mtg_console_test/GameTest.cs
+++ mtg_console/mtg_console_test/GameTest.cs
@@ -60,4 +60,104 @@ public class GameTest
         Assert.Equal(StatusCode.OK, status.GetStatusCode());
         Assert.Null(status.GetInfo());
     }
+
+    [Fact]
+    public void TestNotFoundCard()
+    {
+        // Create the game
+        Game game = new Game(2, Format.CONSTRUCTED);
+
+        // Create a normal deck of 60 cards with a card that doesn't exist
+        string deckList = @"
+            //Main
+            1 Eiganjo Castle
+            1 Eiganjo, Seat of the Empire
+            4 Ghost Quarter
+            4 Hallowed Fountain
+            1 <*_*>
+            4 Plains
+            4 Seachrome Coast
+            2 Volatile Fault
+            4 Aether Vial
+            4 Giver of Runes
+            4 Path to Exile
+            4 Leonin Arbiter
+            2 Pippin, Guard of the Citadel
+            4 Unsettled Mariner
+            4 White Orchid Phantom
+            2 Archon of Emeria
+            2 Skyclave Apparition
+            4 Spell Queller
+            2 Teferi, Time Raveler
+            3 Tishana's Tidebinder
+
+            //Sideboard
+            1 Drannith Magistrate
+            2 Lavinia, Azorius Renegade
+            4 Sanctifier en-Vec
+            3 Suncleanser
+            2 Aven Interrupter
+            2 The Battle of Bywater
+            1 Linvala, Keeper of Silence
+        ";
+
+        // Set the deck
+        Status status = game.SetPlayerDeck(0, deckList);
+
+        // Check
+        Assert.Equal(StatusCode.CARD_NOT_FOUND, status.GetStatusCode());
+
+        List<string> correctAnswer = new List<string> { "<*_*>" };
+        Assert.Equal(correctAnswer, status.GetInfo());
+    }
+
+    [Fact]
+    public void TestNotFoundCardSideboard()
+    {
+        // Create the game
+        Game game = new Game(2, Format.CONSTRUCTED);
+
+        // Create a normal deck of 60 cards with a card that doesn't exist
+        string deckList = @"
+            //Main
+            1 Eiganjo Castle
+            1 Eiganjo, Seat of the Empire
+            4 Ghost Quarter
+            4 Hallowed Fountain
+            1 Island
+            4 Plains
+            4 Seachrome Coast
+            2 Volatile Fault
+            4 Aether Vial
+            4 Giver of Runes
+            4 Path to Exile
+            4 Leonin Arbiter
+            2 Pippin, Guard of the Citadel
+            4 Unsettled Mariner
+            4 White Orchid Phantom
+            2 Archon of Emeria
+            2 Skyclave Apparition
+            4 Spell Queller
+            2 Teferi, Time Raveler
+            3 Tishana's Tidebinder
+
+            //Sideboard
+            1 <*_*>
+            2 Lavinia, Azorius Renegade
+            4 Sanctifier en-Vec
+            3 Suncleanser
+            2 Aven Interrupter
+            2 The Battle of Bywater
+            1 Linvala, Keeper of Silence
+        ";
+
+        // Set the deck
+        Status status = game.SetPlayerDeck(0, deckList);
+
+        // Check
+        Assert.Equal(StatusCode.CARD_NOT_FOUND, status.GetStatusCode());
+
+        List<string> correctAnswer = new List<string> { "<*_*>" };
+        Assert.Equal(correctAnswer, status.GetInfo());
+    }
 }
